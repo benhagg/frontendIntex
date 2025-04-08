ac5fa834d7e9106d96cb0e5467714a846928b3e3
Ben Haggard <21bhaggard@gmail.com>
2025-04-08 14:07:28 -0600

    new version of CSP headers to allow azure blob and localhost root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     headers: {
       "Content-Security-Policy":
-        "default-src 'self'; script-src 'self' 'unsafe-inline' unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-ancestors 'none'; font-src 'self' data:; connect-src 'self' http://localhost:5232/api https://intexbackend-a6fvcvg6cha4hwcx.centralus-01.azurewebsites.net/api; object-src 'none'; base-uri 'self'; form-action 'self'",
+        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://moviepostersintex.blob.core.windows.net; frame-ancestors 'none'; font-src 'self' data:; connect-src 'self' http://localhost:5232 https://intexbackend-a6fvcvg6cha4hwcx.centralus-01.azurewebsites.net; object-src 'none'; base-uri 'self'; form-action 'self'",
     },
   },
 });
